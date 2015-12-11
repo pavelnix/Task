2211a28c17a39100825abe451e9805f3d87b75b4
Pavel <pavelnix@yandex.ru>
2015-12-10 17:50:04 -0800

    Factorial

---
+++ main/java/ru/pavelnix/Factorial.java
@@ -0,0 +1,43 @@
+package ru.pavelnix;
+
+/**
+ * Class Factorial
+ */
+public class Factorial {
+    /**
+     * validate - boolean variable is true if factorial exist and false if not.
+     * number - integer variable factorial of which we want to calculate/
+     */
+    private boolean validate;
+    private int number;
+
+    /**
+     * Constructor
+     */
+    public Factorial(int number) {
+        if (number < 0) {
+            this.validate = false;
+        } else {
+            this.number = number;
+        }
+    }
+
+    /**
+     * Getter for validate
+     */
+    public boolean getValidate() {
+        return this.validate;
+    }
+
+    /**
+     * Method is count factorial
+     */
+    public int countFactorial() {
+        int result = 1;
+        for (int i = 2; i <= this.number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
---
+++ test/java/ru/pavelnix/FactorialTest.java
@@ -0,0 +1,58 @@
+package ru.pavelnix;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Testing class factorial
+ */
+public class FactorialTest {
+
+    /**
+     * The factorial should not exist if the number is negative
+     */
+    @Test
+    public void factorialShouldNotExistIfNumberNegative() {
+        //Arrange
+        Factorial factorial = new Factorial(-1);
+
+        //Act
+        boolean validate = factorial.getValidate();
+
+        //Assert
+        assertFalse(validate);
+    }
+
+    /**
+     * The factorial should equals one if the number is zero
+     */
+    @Test
+    public void factorialShouldOneIfNumberZero() {
+        //Arrange
+        Factorial factorial = new Factorial(0);
+
+        //Act
+        int valueOfFactorial = factorial.countFactorial();
+
+        //Assert
+        assertThat(valueOfFactorial, is(1));
+    }
+
+    /**
+     * The factorial should be counted by the formula 1*2*..*N if the number more zero
+     */
+    @Test
+    public void factorialShouldCountedIfNumberMoreZero() {
+        //Arrange
+        Factorial factorial = new Factorial(10);
+
+        //Act
+        int valueOfFactorial = factorial.countFactorial();
+
+        //Assert
+        assertThat(valueOfFactorial, is(3628800));
+    }
+}
